1bb83916385e43e6f2c20c26c71312a922bf2407
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-08-30 11:01:43 +0200

    set GPU_NUM to 0 in scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
-GPU_NUM=1
-TEST_CONFIG_YAML="configs/base_modl,k=10.yaml"
+GPU_NUM=0
+TEST_CONFIG_YAML="configs/base_modl,k=1.yaml"
 
 CUDA_VISIBLE_DEVICES=$GPU_NUM python test.py \
     --config=$TEST_CONFIG_YAML \
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-GPU_NUM=1
+GPU_NUM=0
 TRAIN_CONFIG_YAML="configs/base_modl,k=1.yaml"
 
 CUDA_VISIBLE_DEVICES=$GPU_NUM python train.py \
