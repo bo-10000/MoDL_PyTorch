572ee54e3e9268dacb17d19cf8d3abd886754a13
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-09-14 11:15:57 +0200

    use SenseOp in modl_dataset

--- datasets/modl_dataset.py
+++ datasets/modl_dataset.py
@@ -4,6 +4,7 @@ import h5py as h5
 import numpy as np
 
 from utils import c2r
+from models import mri
 
 class modl_dataset(Dataset):
     def __init__(self, mode, dataset_path, sigma=0.01):
@@ -23,15 +24,36 @@ class modl_dataset(Dataset):
         """
         with h5.File(self.dataset_path, 'r') as f:
             gt, csm, mask = f[self.prefix+'Org'][index], f[self.prefix+'Csm'][index], f[self.prefix+'Mask'][index]
-        x0 = undersample(gt, csm, mask, self.sigma)
-        # mask = np.stack([mask, mask])
-        # return torch.from_numpy(c2r(x0)), torch.from_numpy(c2r(gt)), torch.from_numpy(c2r(csm)), torch.from_numpy(mask)
+
+        x0 = undersample_(gt, csm, mask, self.sigma)
+
         return torch.from_numpy(c2r(x0)), torch.from_numpy(c2r(gt)), torch.from_numpy(csm), torch.from_numpy(mask)
 
     def __len__(self):
         with h5.File(self.dataset_path, 'r') as f:
             num_data = len(f[self.prefix+'Mask'])
         return num_data
+
+
+def undersample_(gt, csm, mask, sigma):
+
+    ncoil, nrow, ncol = csm.shape
+    csm = csm[None, ...]  # 4dim
+
+    # shift sampling mask to k-space center
+    mask = np.fft.ifftshift(mask, axes=(-2, -1))
+
+    SenseOp = mri.SenseOp(csm, mask)
+
+    b = SenseOp.fwd(gt)
+
+    noise = torch.randn(b.shape) + 1j * torch.randn(b.shape)
+    noise = noise * sigma / (2.**0.5)
+
+    atb = SenseOp.adj(b + noise).squeeze(0).detach().numpy()
+
+    return atb
+
 
 def undersample(gt, csm, mask, sigma):
     """
